23397179c53b79a8dd77d9959cac6655ccbf6e75
lv12312 <lv-12312@163.com>
2012-06-05 09:15:27 +0800

    获取机器的物理地址
    .NET C#

---
+++ GetMacAddress.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * 
+ * 用户： lv
+ * 日期: 2012/5/24
+ * 时间: 8:49
+ * 
+ */
+using System;
+using System.Management;
+namespace ElecFeePoj
+{
+	/// <summary>
+	/// Description of GetMacAddress.
+	/// </summary>
+	public class GetMacAddress
+	{
+		private GetMacAddress()
+		{
+		}
+		public static string getMacAddress(){
+			 ManagementClass mc;     
+			 ManagementObjectCollection moc;
+			 mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+			 moc = mc.GetInstances();     
+			 string str = "";
+			 foreach (ManagementObject mo in moc)     
+			 {         
+			 	if ((bool)mo["IPEnabled"] == true)         
+			 	{            
+			 		str = mo["MacAddress"].ToString();
+			 		break;
+			 	}    
+			 }
+			 return str;
+		}
+	}
+}
